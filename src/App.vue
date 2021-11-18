b5fba0b692691f252bb96169dce1477d2ecdbc9d
lumilio <logistic-division@lumilium.com>
2021-11-18 14:43:20 +0100

    Create arrey and stamp arrey element key on target

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,15 @@
 <template>
   <div id="app">
       <input type="text">
-      <button>click to filter</button>
+      <button @click="Research()">click to filter</button>
+      <div>
+          <div v-for='element in List' :key='element.id'>
+              <p>{{element.title}}</p>
+              <p>{{element.original_title}}</p>
+              <p>{{element.original_language}}</p>
+              <p>{{element.vote_average}}</p>
+          </div>
+      </div>
   </div>
 </template>
 
@@ -21,27 +29,20 @@ export default {
     data() {
         return {
             InputSearch:'ciao',
+            List:[],
         };
     },
-    methods:{}, 
-    mounted() {
-
-
-        var axios = require('axios');
-
-        var config = {
-        method: 'get',
-        url: 'https://api.themoviedb.org/3/search/movie?api_key=aa241e36a559a2927e235d5e8f93f3b5&query='+`${this.InputSearch}`,
-        headers: { }
-        };
-        axios(config)
-        .then(function (response) {
-        console.log(JSON.stringify(response.data));
-        })
-        .catch(function (error) {
-        console.log(error);
-        });      
-    },
+    methods:{
+        Research(){
+            var axios = require('axios');
+            var config = {
+                method: 'get',
+                url: 'https://api.themoviedb.org/3/search/movie?api_key=aa241e36a559a2927e235d5e8f93f3b5&query='+`${this.InputSearch}`,
+                headers: { }
+            };
+            axios(config).then(response => {this.List = response.data.results}).catch(function(error){console.log(error);}); 
+        }
+    }, 
 }    
 </script>
 
