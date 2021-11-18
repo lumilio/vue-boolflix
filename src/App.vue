d54372d72c57355ace3e385c31fcacb048e88de0
lumilio <logistic-division@lumilium.com>
2021-11-18 14:10:12 +0100

    Testing axios request

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <template>
-  <div id="app" class="bg-dark">
-    <img alt="Vue logo" src="./assets/logo.png">
+  <div id="app">
+      <input type="text">
+      <button>click to filter</button>
   </div>
 </template>
 
@@ -9,7 +10,6 @@
 //-----------------utilities--------------------
 import "bootstrap/dist/css/bootstrap.min.css"
 import "bootstrap"
-import axios from 'axios';
 //--------------------------------------------
 //---------------components-------------------
 
@@ -18,16 +18,35 @@ import axios from 'axios';
 export default {
     name: 'App',
     components: {},
+    data() {
+        return {
+            InputSearch:'ciao',
+        };
+    },
+    methods:{}, 
     mounted() {
-        axios
-        .get("https://flynn.boolean.careers/exercises/api/array/music")
-        .then(myResp => {console.log(myResp.data.response); })
-        .catch(e => {console.error(e, 'errore di caricamento');})       
+
+
+        var axios = require('axios');
+
+        var config = {
+        method: 'get',
+        url: 'https://api.themoviedb.org/3/search/movie?api_key=aa241e36a559a2927e235d5e8f93f3b5&query='+`${this.InputSearch}`,
+        headers: { }
+        };
+        axios(config)
+        .then(function (response) {
+        console.log(JSON.stringify(response.data));
+        })
+        .catch(function (error) {
+        console.log(error);
+        });      
     },
 }    
-
-
 </script>
+
+
+
 
 <style lang="scss">
 </style>
---
+++ src/components/Banner1.vue
@@ -0,0 +1,35 @@
+<template>
+    <div class="container-fluid d-flex justify-content-center">
+        <div class="container">
+        </div>
+    </div>
+</template>
+
+/* -------------------------------------------------------------------------- */
+
+<script>
+//-----------------utilities--------------------
+import 'bootstrap-vue/dist/bootstrap-vue.css';
+import 'bootstrap/dist/css/bootstrap.css';
+//--------------------------------------------
+//---------------components-------------------
+// none
+//--------------------------------------------
+
+
+
+export default {
+    name: '',
+    components: {},
+    props: {},
+/*  data() {},
+    methods: {},
+    computed: {},
+    mounted() {}, */
+}
+</script>
+
+/* -------------------------------------------------------------------------- */
+
+<style scoped lang="scss">
+</style>
