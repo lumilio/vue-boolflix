29793ce86256e458df24139328ecc50dc214dee8
lumilio <logistic-division@lumilium.com>
2021-11-18 13:00:18 +0100

    Create vue project + Install Bootstrap , Axios

---
+++ src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <div id="app" class="bg-dark">
+    <img alt="Vue logo" src="./assets/logo.png">
+  </div>
+</template>
+
+<script>
+
+//-----------------utilities--------------------
+import "bootstrap/dist/css/bootstrap.min.css"
+import "bootstrap"
+import axios from 'axios';
+//--------------------------------------------
+//---------------components-------------------
+
+//--------------------------------------------
+
+export default {
+    name: 'App',
+    components: {},
+    mounted() {
+        axios
+        .get("https://flynn.boolean.careers/exercises/api/array/music")
+        .then(myResp => {console.log(myResp.data.response); })
+        .catch(e => {console.error(e, 'errore di caricamento');})       
+    },
+}    
+
+
+</script>
+
+<style lang="scss">
+</style>
