558103253c48bd533a0e3f0bac132a189f991984
lumilio <logistic-division@lumilium.com>
2021-11-24 22:38:55 +0100

    Fix 404 img case

--- src/components/CardBox.vue
+++ src/components/CardBox.vue
@@ -12,7 +12,8 @@
         <div class="container d-flex flex-wrap justify-content-center">
             <!----------- card -->
             <div v-for='(element, index) in CardList' :key='index' @mouseleave="ShowData = false" class="card-box" v-bind:class ="(element.genre_ids.includes(SelectValue) || SelectValue == 'All' )?'':'d-none'">
-                <img :src="'https://image.tmdb.org/t/p/w342/' + element.poster_path" :alt="(element.name == undefined)? element.title : element.name" class="card-image">
+                <img v-if='element.poster_path != undefined' :src="'https://image.tmdb.org/t/p/w342/' + element.poster_path" :alt="(element.name == undefined)? element.title : element.name" class="card-image">
+                <img v-else src="../assets/img/404pic.png" :alt="(element.name == undefined)? element.title : element.name" class="card-image">
                 <div class="card-data">
                     <!----------- nome o titolo -->
                     <template v-if='element.name == undefined'>
